8be6cce809e71869807088430cbc8e0890eb082c
UgAr-study <uglovskij.au@gmail.com>
2021-03-09 23:32:19 +0300

    TCPSender updated for MessageObject

--- app/src/main/java/com/example/p2pchat/ui/chat/MessageItem.java
+++ app/src/main/java/com/example/p2pchat/ui/chat/MessageItem.java
@@ -12,6 +12,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class MessageItem {
+
     private final String name;
     private final Calendar date;
     private final String message;
